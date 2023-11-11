4b641cad2ecd92fc64800519f7464852b5738b24
0511Cynthia <223205@ids.upchiapas.edu.mx>
2023-11-11 16:10:27 -0600

    change

--- app/page.js
+++ app/page.js
@@ -2,15 +2,6 @@
 // import PedidosTotales from '@/src/app/pedidosTotales/page'
 
 export default function MyPage() {
-  const [showModal, setShowModal] = useState(false);
-
-  const openModal = () => {
-    setShowModal(true);
-  };
-
-  const closeModal = () => {
-    setShowModal(false);
-  };
 
   return (
     <div>
